708c92d1f2043f2ccfea3a434450ed2161b081d0
Dave Tyson <davetysonbass@gmail.com>
2023-05-21 15:48:28 -0400

    set up a localStorage method for login, 3/4 complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,27 +5,28 @@ import logo from '../assets/ramonesGeneratorLogo.png'
 import NavBar from './NavBar';
 // import LoginForm from './LoginForm';
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { getAuth, signOut } from "firebase/auth";
 // If I need the emulator I can bring it back
 // connectAuthEmulator, signInWithEmailAndPassword, createUserWithEmailAndPassword
 
 const Header = () => {
 
     // TO DO
-    // Because of the weirdness of passing state values between links, I'm going to move all the login/auth stuff for firebase to the login form instead of having it in the header here
-    // The Header will only have the login button and link to it like any other Nav link
-    // Once I move all this firebase stuff to the login form, the input states will work again
-    // I can run all the functions to check the login data when the user submits their form
+    // Because of the weirdness of passing state values between links, I'm going to move all the login/auth stuff for firebase to the login form instead of having it in the header here - DONE
+    // The Header will only have the login button and link to it like any other Nav link - DONE
+    // Once I move all this firebase stuff to the login form, the input states will work again - DONE
+    // I can run all the functions to check the login data when the user submits their form - DONE
     // Depending on what happens, render different html
-    // Store the user ID in the local storage and see if you can use that to access later when they click on the "my songs" area, and pull it into a new state from there, rather than trying to pass the same state down through links
+    // Store the user ID in the local storage and see if you can use that to access later when they click on the "my songs" area, and pull it into a new state from there, rather than trying to pass the same state down through links 
     // Clean up the header and login form code to tidy up
 
     // useStates
     const [ loggedIn, setLoggedIn ] = useState(false);
-    const [ loginError , setLoginError ] = useState("");
+
+    // const [ loginError , setLoginError ] = useState("");
     // const [ loginWindow , setLoginWindow ] = useState(false);
-    const [ email , setEmail ] = useState("");
-    const [ password , setPassword ] = useState("");
+    // const [ email , setEmail ] = useState("");
+    // const [ password , setPassword ] = useState("");
 
 
     // initial firebase config
@@ -42,54 +43,82 @@ const Header = () => {
     const app = initializeApp(firebaseConfig);
     const auth = getAuth(app);
 
+    const logOut = async () => {
+        const loggedOutMsg = await signOut(auth);
+        console.log(loggedOutMsg);
+        setLoggedIn(false);
+        localStorage.setItem("email", "");
+        localStorage.setItem("userID", "");
+    };
+
     // Emulators
     // connectAuthEmulator(auth, "https://localhost:9099");
 
     // logAttempt function
-    const logAttempt = async (e) => {
-        e.preventDefault();
-        if (loggedIn === false) {
-            // test data
-            const loginEmail = email;
-            const loginPassword = password;
+    // const logAttempt = async (e) => {
+    //     e.preventDefault();
+    //     if (loggedIn === false) {
+    //         // test data
+    //         const loginEmail = email;
+    //         const loginPassword = password;
 
-            // call 
-            const auth = getAuth(app);
-            await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
-            .then((userCredential) => {
-                const user = userCredential.user;
-                setLoggedIn(true);
-                console.log(user);
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                setLoggedIn(false);
-                setLoginError(errorCode);
-                console.log(errorCode);
-                console.log(errorMessage);
-            });
-        } else {
-            const loggedOutMsg = await signOut(auth);
-            console.log(loggedOutMsg);
-            setLoggedIn(false);
-        }
-    }
+    //         // call 
+    //         const auth = getAuth(app);
+    //         await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+    //         .then((userCredential) => {
+    //             const user = userCredential.user;
+    //             setLoggedIn(true);
+    //             console.log(user);
+    //         })
+    //         .catch((error) => {
+    //             const errorCode = error.code;
+    //             const errorMessage = error.message;
+    //             setLoggedIn(false);
+    //             setLoginError(errorCode);
+    //             console.log(errorCode);
+    //             console.log(errorMessage);
+    //         });
+    //     } else {
+    //         const loggedOutMsg = await signOut(auth);
+    //         console.log(loggedOutMsg);
+    //         setLoggedIn(false);
+    //     }
+    // }
 
-    // this just console.logs the loggedIn status so I know what it is while testing
+    // // this just console.logs the loggedIn status so I know what it is while testing
+    // // useEffect(() => {
+    // //     console.log(loggedIn);
+    // // }, [loggedIn])
+
     // useEffect(() => {
-    //     console.log(loggedIn);
-    // }, [loggedIn])
+    //     console.log(loginError);
+    //     // create some code here that does this
+    //     // if the login error is null or blank (i.e. page load) do nothing
+    //     // if the login error is about the email, flash that the email is wrong
+    //     // if the login error is about the password, flash that the password is wrong
+    //     // if the login error is about anything else, flash try again
+    //     // see about how you include a password reset link
+    // }, [loginError])
+
+    // setLoggedIn(false);
 
     useEffect(() => {
-        console.log(loginError);
-        // create some code here that does this
-        // if the login error is null or blank (i.e. page load) do nothing
-        // if the login error is about the email, flash that the email is wrong
-        // if the login error is about the password, flash that the password is wrong
-        // if the login error is about anything else, flash try again
-        // see about how you include a password reset link
-    }, [loginError])
+        const userEmail = localStorage.getItem("userEmail");
+        const userID = localStorage.getItem("userID");
+        console.log(userEmail);
+        console.log(userID);
+
+        if (userEmail === null) {
+            console.log(userEmail);
+            setLoggedIn(false);
+        } else {
+            setLoggedIn(true);
+            console.log(userID);
+        }
+    }, []);
+
+    console.log(loggedIn);
+
 
     return (
         <>
@@ -102,24 +131,10 @@ const Header = () => {
                     </figure>
                     </NavLink>
                 </div>
-                <NavLink to="/login" className="hover:text-customGreen focus:text-customGreen transition text-center">
-                    <button className="p-2 mt-5 md:mt-0 border-4 border-white bg-customGreen rounded-md text-lg sm:text-2xl text-black font-bold lg:absolute lg:right-14 lg:top-10 transition hover:text-customGreen focus:text-customGreen hover:bg-black focus:bg-black">{loggedIn === false ? `Log In` : `Log Out`}</button>
-                    {/* <Login 
-                        loginEmailPassword={loginEmailPassword}
-                        logOut={logOut}
-                        loggedIn={loggedIn}
-                        setLoginWindow={setLoginWindow}
-                    /> */}
+                <NavLink to={loggedIn === false ? "/login" : null}>
+                    <button className="p-2 mt-5 md:mt-0 border-4 border-white bg-customGreen rounded-md text-lg sm:text-2xl text-black font-bold lg:absolute lg:right-14 lg:top-10 transition hover:text-customGreen focus:text-customGreen hover:bg-black focus:bg-black" onClick={loggedIn === true ? logOut : null}>{loggedIn === false ? `Log In` : `Log Out`}</button>
                 </NavLink>
                 <NavBar />
-                {/* {loginWindow === true ? 
-                    <LoginForm 
-                        email={email}
-                        setEmail={setEmail}
-                        password={password}
-                        setPassword={setPassword}
-                        logAttempt={logAttempt}
-                    /> : <></>} */}
             </header> 
         </>
     )
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,15 +1,92 @@
-// import { useEffect } from "react";
-// import { NavLink } from "react-router-dom";
-// import Login from "./Login";
+import { useState, useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+// If I need the emulator I can bring it back
+// connectAuthEmulator, signInWithEmailAndPassword, createUserWithEmailAndPassword
 
 const LoginForm = (props) => {
 
+    // useStates
+    const [ loggedIn, setLoggedIn ] = useState(false);
+    const [ loginError , setLoginError ] = useState("");
+    const [ email , setEmail ] = useState("");
+    const [ password , setPassword ] = useState("");
+
+
+    // initial firebase config
+    const firebaseConfig = {
+    apiKey: "AIzaSyCggW88fy90jEAac9ygi33b-XL4gZsAtIk",
+    authDomain: "ramones-generator.firebaseapp.com",
+    projectId: "ramones-generator",
+    storageBucket: "ramones-generator.appspot.com",
+    messagingSenderId: "295603373754",
+    appId: "1:295603373754:web:e9f185081077c9b863e483"
+    };
+
+    // initialize firebase app and auth
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+
+    // Emulators
+    // connectAuthEmulator(auth, "https://localhost:9099");
+
+    // logAttempt function
+    const logAttempt = async (e) => {
+        e.preventDefault();
+        if (loggedIn === false) {
+            const loginEmail = email;
+            const loginPassword = password;
+
+            // call 
+            const auth = getAuth(app);
+            await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                const userID = user.auth.currentUser.uid;
+                const userEmail = user.auth.currentUser.email;
+                setLoggedIn(true);
+                console.log(userEmail);
+                console.log(userID);
+                localStorage.setItem("userEmail", userEmail);
+                localStorage.setItem("userID", userID);
+                console.log(user);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                setLoggedIn(false);
+                setLoginError(errorCode);
+                console.log(errorCode);
+                console.log(errorMessage);
+            });
+        } else {
+            const loggedOutMsg = await signOut(auth);
+            console.log(loggedOutMsg);
+            setLoggedIn(false);
+        }
+    }
+
+    // this just console.logs the loggedIn status so I know what it is while testing
+    useEffect(() => {
+        console.log(loggedIn);
+    }, [loggedIn])
+
+    useEffect(() => {
+        console.log(loginError);
+        // create some code here that does this
+        // if the login error is null or blank (i.e. page load) do nothing
+        // if the login error is about the email, flash that the email is wrong
+        // if the login error is about the password, flash that the password is wrong
+        // if the login error is about anything else, flash try again
+        // see about how you include a password reset link
+    }, [loginError])
+
     const handleEmailChange = (e) => {
-        props.setEmail(e.target.value);
+        setEmail(e.target.value);
     }
 
     const handlePasswordChange = (e) => {
-        props.setPassword(e.target.value);
+        setPassword(e.target.value);
     }
 
     return (
@@ -18,7 +95,7 @@ const LoginForm = (props) => {
                 <h2 className="mb-10 uppercase quantico w-full text-4xl font-bold text-customGreen">Log In</h2>
                 <p className="mb-5 text-lg lg:w-2/3 inline-block mx-auto">Log in to your account to save new songs and view your saved favorites!</p>
                 <div>
-                    <form onSubmit={()=>{props.logAttempt()}}>
+                    <form onSubmit={logAttempt}>
                         <fieldset className="flex flex-col items-center">
                             <label className="sr-only" htmlFor="email">Email</label>
                             <input className="text-white text-center lg:text-2xl text-l border-4 border-white mb-5 p-1 quantico rounded-md bg-black w-1/2" id="email" type="email" placeholder="Email" onChange={handleEmailChange} value={props.email} />
@@ -34,29 +111,3 @@ const LoginForm = (props) => {
 }
 
 export default LoginForm;
-
-// import { useEffect } from "react";
-
-// const Input = (props) => {
-
-//     const handleChange = (e) => {
-//         props.setActivity(e.target.value);
-//     }
-
-//     useEffect(() => {
-
-//     }, [props.activity])
-
-//     return (
-//         <>
-//             <div className="flex flex-col">
-//                 <h3 className="mb-5 text-xl quantico underline">Question 1</h3>
-//                 <p className="mb-1 text-lg">What are you doing in the song?</p>
-//                 <label className="mb-5 text-sm" htmlFor="activity">Enter any activity (i.e. going to the basement, being sedated, going to rockaway beach, etc).</label>
-//                 <input className="text-white text-center lg:text-2xl text-l border-4 border-black mb-5 p-1 quantico rounded-md bg-black" id="activity" type="text" name="activity" placeholder="Enter your activity here" onChange={handleChange} value={props.activity}/>
-//             </div>
-//         </>
-//     )
-// }
-
-// export default Input;
