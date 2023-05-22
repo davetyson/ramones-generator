a66d0d6326abdc79dd05ed71e28fc77d130ff24a
Dave Tyson <davetysonbass@gmail.com>
2023-05-22 16:07:16 -0400

    refactored LoginForm.js a bit, password reset fully works, links between login parts all work

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-// import { NavLink } from "react-router-dom";
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
 // If I need the emulator I can bring it back
@@ -16,7 +15,10 @@ const LoginForm = (props) => {
     const [ signingUp , setSigningUp ] = useState(false);
     const [ forgotPassword, setForgotPassword ] = useState(false);
     const [ resetSent, setResetSent ] = useState(false);
+    const [ actionText, setActionText ] = useState("");
+    const [ infoP, setInfoP ] = useState("");
 
+    // FIREBASE INIT
     // initial firebase config
     const firebaseConfig = {
     apiKey: "AIzaSyCggW88fy90jEAac9ygi33b-XL4gZsAtIk",
@@ -29,59 +31,83 @@ const LoginForm = (props) => {
 
     // initialize firebase app and auth
     const app = initializeApp(firebaseConfig);
-    // const auth = getAuth(app);
 
     // Emulators
     // connectAuthEmulator(auth, "https://localhost:9099");
 
-    // logAttempt function
-    const logAttempt = async (e) => {
-        e.preventDefault();
+    // FIREBASE CALL FUNCTION
+    const fbAction = async (fbCall) => {
+        // e.preventDefault();
         const loginEmail = email.trim();
         const loginPassword = password.trim();
-
-        // call 
-        const auth = getAuth(app);
-        await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
-        .then((userCredential) => {
-            const user = userCredential.user;
-            const userID = user.auth.currentUser.uid;
-            const userToken = user.auth.currentUser.accessToken;
-            setLoggedIn(true);
-            console.log(userToken);
-            console.log(userID);
-            localStorage.setItem("userToken", userToken);
-            localStorage.setItem("userID", userID);
-            window.dispatchEvent(new Event('storage'))
-            console.log(user);
-            setEmail("");
-            setPassword("");
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            setLoggedIn(false);
-            setLoginError(errorCode);
-            console.log(errorCode);
-            console.log(errorMessage);
-        });
-    }
-
-    const loginCheck = () => {
-        const userToken = localStorage.getItem("userToken");
-        const userID = localStorage.getItem("userID");
-        console.log(userToken);
-        console.log(userID);
-
-        if (userToken === "") {
-            console.log(userToken);
-            setLoggedIn(false);
+        if (fbCall === sendPasswordResetEmail) {
+            const auth = getAuth(app);
+            await sendPasswordResetEmail(auth, email.trim())
+            .then(() => {
+                setForgotPassword(false);
+                setResetSent(true);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                setLoginError(errorCode);
+                console.log(errorCode);
+                console.log(errorMessage);
+                setResetSent(false);
+            });
         } else {
-            setLoggedIn(true);
-            console.log(userID);
+            // call 
+            const auth = getAuth(app);
+            await fbCall(auth, loginEmail, loginPassword)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                const userID = user.auth.currentUser.uid;
+                const userToken = user.auth.currentUser.accessToken;
+                setLoggedIn(true);
+                console.log(userToken);
+                console.log(userID);
+                localStorage.setItem("userToken", userToken);
+                localStorage.setItem("userID", userID);
+                window.dispatchEvent(new Event('storage'))
+                console.log(user);
+                setEmail("");
+                setPassword("");
+                if (signingUp === true) {
+                    setSigningUp(false);
+                }
+                setLoggedIn(true);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                setLoggedIn(false);
+                setLoginError(errorCode);
+                console.log(errorCode);
+                console.log(errorMessage);
+            });
         }
     };
 
+    // check which fb action to run when button is clicked
+    const checkFormAction = (e) => {
+        e.preventDefault();
+        if (forgotPassword === true) {
+            fbAction(sendPasswordResetEmail);
+        } else if (signingUp === true) {
+            fbAction(createUserWithEmailAndPassword);
+        } else {
+            fbAction(signInWithEmailAndPassword);
+        }
+    };
+
+    // reset of states after password reset sent successfully
+    const resetDefaults = () => {
+        setSigningUp(false);
+        setForgotPassword(false);
+        setResetSent(false);
+    }
+
+    // controlled inputs
     const handleEmailChange = (e) => {
         setEmail(e.target.value);
     };
@@ -90,89 +116,31 @@ const LoginForm = (props) => {
         setPassword(e.target.value);
     };
 
-    const createAccount = async (e) => {
-        e.preventDefault();
-        const newEmail = email.trim();
-        const newPassword = password.trim();
-        // call 
-        const auth = getAuth(app);
-        await createUserWithEmailAndPassword(auth, newEmail, newPassword)
-        .then((userCredential) => {
-            const user = userCredential.user;
-            const userID = user.auth.currentUser.uid;
-            const userToken = user.auth.currentUser.accessToken;
-            setLoggedIn(true);
-            console.log(userToken);
-            console.log(userID);
-            localStorage.setItem("userToken", userToken);
-            localStorage.setItem("userID", userID);
-            window.dispatchEvent(new Event('storage'))
-            console.log(user);
-            setEmail("");
-            setPassword("");
-            setSigningUp(false);
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            setLoggedIn(false);
-            setLoginError(errorCode);
-            setSigningUp(true);
-            console.log(errorCode);
-            console.log(errorMessage);
-        });
-    }
+        // useEffect to determine h2 text
+        useEffect(() => {
+            console.log(signingUp);
+            console.log(loggedIn);
+            console.log(forgotPassword);
+            if (loggedIn === true) {
+                setActionText("You are now logged in");
+                setInfoP("Congratulations! You are now logged in. Head to the Generator to start generating songs!");
+            } else if (forgotPassword === true) {
+                setActionText("Reset Password");
+                setInfoP("Enter your email to reset your password.");
+            } else if (signingUp === true) {
+                setActionText("Sign Up");
+                setInfoP("Sign up for an account to save your favorite songs!");
+            } else if (resetSent === true) {
+                setActionText("Your Password Reset Email Has Been Sent");
+                setInfoP("Please check your email inbox to reset your password!")
+            } else {
+                setActionText("Log In");
+                setInfoP("Log in to your account to save new songs and view your saved favorites!");
+            }
+        }, [forgotPassword, loggedIn, signingUp, resetSent]);
 
-    const resetPassword = async () => {
-        setForgotPassword(true);
-        const auth = getAuth(app);
-        sendPasswordResetEmail(auth, email.trim())
-        .then(() => {
-            // Password reset email sent!
-            setResetSent(true);
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            setLoginError(errorCode);
-            console.log(errorCode);
-            console.log(errorMessage);
-            setResetSent(false);
-            // ..
-        });
-    };
-
-    const checkFormAction = () => {
-        if (forgotPassword === true) {
-            resetPassword();
-        } else if (signingUp === true) {
-            createAccount();
-        } else {
-            logAttempt();
-        }
-    };
-
-    useEffect(() => {
-        window.addEventListener('storage', loginCheck);
-
-        return () => {
-            window.removeEventListener('storage', loginCheck);
-        };
-    }, []);
-
-    // this just console.logs the loggedIn status so I know what it is while testing
-    useEffect(() => {
-        console.log(loggedIn);
-    }, [loggedIn]);
-
-    // useEffect(() => {
-    //     resetPassword();
-    //     // eslint-disable-next-line
-    // }, [signingUp]);
-
-    useEffect(() => {
-        console.log(loginError);
-        
+    // check for an error message and determine which one to display to the user if one exists
+    useEffect(() => {        
         if (loginError === "") {
             setErrorMsg("");
         } else if (loginError === "auth/email-already-in-use") {
@@ -193,24 +161,43 @@ const LoginForm = (props) => {
         }, 4000);
     }, [loginError]);
 
-    // Will need to keep working on this to make sure that the reset password functions work
-    // Right now the navigation between log in and sign up works perfectly, you can move between all those links just fine and everything renders as it should
-    // When you click in to reset your password, and then click the sign up button, it doesn't re-render the h2, the text, or the form, so I need to work on this
-    // Will likely have to take all the spots where I'm checking for three things (are we logging in, signing up, or did we forget our password) and create functions for them, like I did for the form submission
-    // Check for certain state conditions and then if they exist, put certain information into a variable, and use that variable in the rendered html
-    // Will be able to remove most of the ternary's after moving things into functions, and then things should work well
+    // this just console.logs the loggedIn status so I know what it is while testing
+    useEffect(() => {
+        console.log(loggedIn);
+    }, [loggedIn]);
+
+    console.log(resetSent);
+
+    // the below function/useEffect are the beginnings of a check to see if we are already logged in if this page is loaded independently. once I have a richer system of verifying logins in place, I can use those same calls to check things out here and modify the below function/useEffect as needed. commenting out for now
+
+    // use this to see if the user is logged in already on page load
+    // const loginCheck = () => {
+    //     const userToken = localStorage.getItem("userToken");
+    //     const userID = localStorage.getItem("userID");
+    //     console.log(userToken);
+    //     console.log(userID);
+
+    //     if (userToken === "") {
+    //         console.log(userToken);
+    //         setLoggedIn(false);
+    //     } else {
+    //         setLoggedIn(true);
+    //         console.log(userID);
+    //     }
+    // };
+
+    // useEffect(() => {
+    //     window.addEventListener('storage', loginCheck);
+
+    //     return () => {
+    //         window.removeEventListener('storage', loginCheck);
+    //     };
+    // }, []);
 
     return (
         <>
             <section className="p-10">
-                {loggedIn === false ?
-                    forgotPassword === false ?
-                        <h2 className="uppercase quantico mb-5 w-full text-4xl font-bold text-customGreen">{signingUp === false ? `Log In` : `Sign Up`}</h2>
-                    :
-                        <h2 className="uppercase quantico mb-5 w-full text-4xl font-bold text-customGreen">Reset Password</h2>
-                : 
-                    <h2 className="uppercase quantico mb-5 w-full text-4xl font-bold text-customGreen">You are logged in</h2>
-                }
+                <h2 className="uppercase quantico mb-5 w-full text-4xl font-bold text-customGreen">{actionText}</h2>
                 {loginError !== "" ?
                 <div>
                     <p className="mx-auto my-2 p-2 w-1/2 bg-white text-black rounded-lg quantico text-lg transition animate-fade">{errorMsg}</p>
@@ -219,44 +206,51 @@ const LoginForm = (props) => {
                 }
                 { loggedIn === false ?
                 <>
-                {forgotPassword === false ?
-                    <p className="mb-5 text-lg lg:w-2/3 inline-block mx-auto">{signingUp === false ? `Log in to your account to save new songs and view your saved favorites!` : `Sign up for an account to save your favorite songs!`}</p>
-                : 
-                    <p className="mb-5 text-lg lg:w-2/3 inline-block mx-auto">{resetSent === false ? `Enter your email to reset your password` : `Password reset sent! Please check your email to reset your password.`}</p>
-                }
-                    <form onSubmit={checkFormAction}>
-                        <fieldset className="flex flex-col items-center">
-                            <label className="sr-only" htmlFor="email">Email</label>
-                            <input className="text-white text-center lg:text-2xl text-lg border-4 border-white mb-5 p-1 quantico rounded-md bg-black w-full md:w-1/2" id="email" type="email" placeholder="Email" onChange={handleEmailChange} value={email} />
-                            {forgotPassword === false ?
-                            <>
-                                <label className="sr-only" htmlFor="password">Password</label>
-                                <input className="text-white text-center lg:text-2xl text-lg border-4 border-white mb-5 p-1 quantico rounded-md bg-black w-full md:w-1/2" id="password" type="password" placeholder="Password" onChange={handlePasswordChange} value={password} />
-                            </>
-                            : <></>
-                            }
-                        </fieldset>
-                        {forgotPassword === false ?
-                            <button type="submit" className="p-2 mt-5 md:mt-0 border-4 border-white bg-customGreen rounded-md text-lg sm:text-2xl text-black font-bold transition hover:text-customGreen focus:text-customGreen hover:bg-black focus:bg-black">{signingUp === false ? `Log In` : `Sign Up`}</button>
-                        : 
-                            <button type="submit" className="p-2 mt-5 md:mt-0 border-4 border-white bg-customGreen rounded-md text-lg sm:text-2xl text-black font-bold transition hover:text-customGreen focus:text-customGreen hover:bg-black focus:bg-black">Reset Password</button>
-                        }
-                    </form>
-                    {signingUp === false ? 
-                        forgotPassword === false ?
-                        <p className="mt-5 text-lg lg:w-2/3 inline-block mx-auto">Forgot your password? <button className="underline" onClick={()=>{setForgotPassword(true)}}>Click here</button> to reset your password.</p>
-                        : <></>
-                        :
+                    <p className="mb-5 text-lg lg:w-2/3 inline-block mx-auto">{infoP}</p>
+                    {resetSent === false ?
+                        <form onSubmit={(e)=>{checkFormAction(e)}}>
+                            <fieldset className="flex flex-col items-center">
+                                <label className="sr-only" htmlFor="email">Email</label>
+                                <input className="text-white text-center lg:text-2xl text-lg border-4 border-white mb-5 p-1 quantico rounded-md bg-black w-full md:w-1/2" id="email" type="email" placeholder="Email" onChange={handleEmailChange} value={email} />
+                                {forgotPassword === false ?
+                                <>
+                                    <label className="sr-only" htmlFor="password">Password</label>
+                                    <input className="text-white text-center lg:text-2xl text-lg border-4 border-white mb-5 p-1 quantico rounded-md bg-black w-full md:w-1/2" id="password" type="password" placeholder="Password" onChange={handlePasswordChange} value={password} />
+                                </>
+                                : <></>
+                                }
+                            </fieldset>
+                            <button type="submit" className="p-2 mt-5 md:mt-0 border-4 border-white bg-customGreen rounded-md text-lg sm:text-2xl text-black font-bold transition hover:text-customGreen focus:text-customGreen hover:bg-black focus:bg-black">{actionText}</button>
+                        </form>
+                    : 
                         <></>
                     }
-                    {signingUp === false ? 
-                        <p className="mt-5 text-lg lg:w-2/3 inline-block mx-auto">Don't have an account? <button className="underline" onClick={()=>{setSigningUp(true)}}>Sign up for one here</button> to save your favorite songs!</p>
-                        :
-                        <p className="mt-5 text-lg lg:w-2/3 inline-block mx-auto">Already have an account? <button className="underline" onClick={()=>{setSigningUp(false)}}>Click here</button> to return to the log in page.</p>
+                    {resetSent === false ?
+                        <>
+                            {signingUp === false ? 
+                                forgotPassword === false ?
+                                <p className="mt-5 text-lg lg:w-2/3 inline-block mx-auto">Forgot your password? <button className="underline" onClick={()=>{setForgotPassword(true)}}>Click here</button> to reset your password.</p>
+                                : 
+                                    <></>
+                            :
+                                <></>
+                            }
+                            {signingUp === false ? 
+                                forgotPassword === false ?
+                                    <p className="mt-5 text-lg lg:w-2/3 inline-block mx-auto">Don't have an account? <button className="underline" onClick={()=>{setSigningUp(true)}}>Sign up for one here</button> to save your favorite songs!</p>
+                                :
+                                    <p className="mt-5 text-lg lg:w-2/3 inline-block mx-auto">Remember your password? <button className="underline" onClick={()=>{setForgotPassword(false)}}>Click here</button> to return to the log in page.</p>
+                            :
+                                <p className="mt-5 text-lg lg:w-2/3 inline-block mx-auto">Already have an account? <button className="underline" onClick={()=>{setSigningUp(false)}}>Click here</button> to return to the log in page.</p>
+                            }
+                        </>
+                    :
+                        <p className="text-lg lg:w-2/3 inline-block mx-auto"><button className="underline" onClick={()=>{resetDefaults()}}>Click here</button> to try your log in again.</p>
                     }
-                </> :
-                <p className="mb-5 text-lg lg:w-2/3 inline-block mx-auto">Congratulations! You are now logged in! Head to the Generator to start generating songs!</p>
-                }
+                </>
+            :
+                <p className="mb-5 text-lg lg:w-2/3 inline-block mx-auto">{infoP}</p>
+            }
             </section>
         </>
     )
